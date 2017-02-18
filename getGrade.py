fbe35225f1492e9ef3e604637ac44ca5d8dcff7b
Kyle Polley <kpolley@outlook.com>
2017-02-18 13:03:41 -0800

    prints nicer. next up is send via fb message

--- getGrade.py
+++ getGrade.py
@@ -1,6 +1,8 @@
+from __future__ import print_function
 import os
 import urllib
 
+#gets PDF from website and converts to ASCII
 def getPdf():
     testfile = urllib.URLopener()
     testfile.retrieve("http://csiflabs.cs.ucdavis.edu/~ssdavis/60/60w17.pdf", "60w17.pdf")
@@ -8,6 +10,7 @@ def getPdf():
     output="/home/pi/Documents/GitHub/grades/out.txt"
     os.system(("ps2ascii %s %s") %( '60w17.pdf', output))
 
+#gets the timestamp of PDF
 def checkTime():
     f = open('out.txt')
     allGrades = f.read()
@@ -15,41 +18,40 @@ def checkTime():
     print(firstLine)
     return firstLine
 
+#finds my ID, prints my grades
 def myGrade():
     f = open('out.txt')
-    
 
-    for i in f:
-        z = 0
-        found = 0
-        while(z != 5000):
-            if(i[z:z+4] == '3794'):
+    
+    for line in f:
+        char = 0
+        found = 0 
+        while(char != 5000):
+            if(line[char:char+4] == '3794'): #finds my ID
                 found = 1;
                 break
-            z=z+1
+            char=char+1
         
         if(found == 1):
-            while(i[z].isalpha() == False):
-                print(i[z])
-                z=z+1
-            
-            print(i[z] + i[z+1])
-            break
+            while(line[char].isalpha() == False): #prints all characters until it hits a letter (my grade)
+                print(line[char], end='')
+                char=char+1
+                
+            #prints grade    
+            if(line[char+1] == '+' or line[char+1] == '-'):
+                print(line[char] + line[char+1])
 
-            
-
-            
-        
-    
+            else:
+                print(line[char])
 
 def main():
-    cts = checkTime()
+    cts = checkTime() #gets current download timestamp
 
-    getPdf()
+    getPdf() #downloads pdf from URL
 
-    nts = checkTime()
+    nts = checkTime() #gets new download timestamp
 
-    if(cts == nts):
+    if(cts == nts):  #if new pdf is updated, print my grade
         myGrade()
 
     
